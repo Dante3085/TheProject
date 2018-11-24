c2c3194e7d84d2fceb4a3e81e413debe9e6346d2
mjsch <moritz97.s@web.de>
2018-11-24 17:55:36 +0100

    AnimatedSprite Showcase 2

--- main.cpp
+++ main.cpp
@@ -15,17 +15,17 @@ int main()
 {
 	InputManager manager{};
 
-	sf::RenderWindow window{ sf::VideoMode{800, 600}, "TheProject" };
+	sf::RenderWindow window{ sf::VideoMode{1920, 1080}, "TheProject" };
 
-	AnimatedSprite* gothicHero = new AnimatedSprite{ "Ressources/gothic-hero-run.png", 66, 48, {100.0f, 0.0f}, 200, .09f };
+	AnimatedSprite* gothicHero = new AnimatedSprite{ "Ressources/gothic-hero-run.png", 66, 48, {100.0f, 0.0f}, 300, .09f };
 	gothicHero->addAnimation(Idle, 12, 0, 0);
-	gothicHero->addAnimation(GoLeft, 5, 37, 1);
-	gothicHero->addAnimation(GoUp, 5, 37, 1);
-	gothicHero->addAnimation(GoRight, 5, 37, 1);
-	gothicHero->addAnimation(GoDown, 5, 37, 1);
-	gothicHero->addAnimation(Attack, 5, 37, 1);
+	gothicHero->addAnimation(GoLeft, 12, 0, 0);
+	gothicHero->addAnimation(GoUp, 12, 0, 0);
+	gothicHero->addAnimation(GoRight, 12, 0, 0);
+	gothicHero->addAnimation(GoDown, 12, 0, 0);
+	gothicHero->addAnimation(Attack, 12, 0, 0);
 
-	AnimatedSprite* demonAttack = new AnimatedSprite{ "Ressources/demon-attack.png", 240, 192, {-150.0f, 50.0f}, 200, .2f };
+	AnimatedSprite* demonAttack = new AnimatedSprite{ "Ressources/demon-attack.png", 240, 192, {150.0f, 200.0f}, 200, .2f };
 	demonAttack->addAnimation(Idle, 11, 0, 0);
 	demonAttack->addAnimation(GoLeft, 5, 37, 1);
 	demonAttack->addAnimation(GoUp, 5, 37, 1);
@@ -33,7 +33,7 @@ int main()
 	demonAttack->addAnimation(GoDown, 5, 37, 1);
 	demonAttack->addAnimation(Attack, 5, 37, 1);
 
-	AnimatedSprite* hellBeast = new AnimatedSprite{ "Ressources/hell-beast-breath.png", 64, 64, {400.f, 50.0f}, 200, .45f };
+	AnimatedSprite* hellBeast = new AnimatedSprite{ "Ressources/hell-beast-breath.png", 64, 64, {400.f, 50.0f}, 200.f, .45f };
 	hellBeast->addAnimation(Idle, 4, 0, 0);
 	hellBeast->addAnimation(GoLeft, 5, 37, 1);
 	hellBeast->addAnimation(GoUp, 5, 37, 1);
@@ -41,7 +41,23 @@ int main()
 	hellBeast->addAnimation(GoDown, 5, 37, 1);
 	hellBeast->addAnimation(Attack, 5, 37, 1);
 
-	std::vector<Entity*>* entities = new std::vector<Entity*>{ gothicHero, demonAttack, hellBeast };
+	AnimatedSprite* ghost = new AnimatedSprite{ "Ressources/ghost-idle.png", 64, 80, {600.f, 50.0f}, 200.f, .15f };
+	ghost->addAnimation(Idle, 7, 0, 0);
+	ghost->addAnimation(GoLeft, 5, 37, 1);
+	ghost->addAnimation(GoUp, 5, 37, 1);
+	ghost->addAnimation(GoRight, 5, 37, 1);
+	ghost->addAnimation(GoDown, 5, 37, 1);
+	ghost->addAnimation(Attack, 5, 37, 1);
+
+	AnimatedSprite* breathFire = new AnimatedSprite{ "Ressources/breath-fire.png", 160, 96, {600.f, 100.0f}, 200.f, .25f };
+	breathFire->addAnimation(Idle, 5, 0, 0);
+	breathFire->addAnimation(GoLeft, 5, 37, 1);
+	breathFire->addAnimation(GoUp, 5, 37, 1);
+	breathFire->addAnimation(GoRight, 5, 37, 1);
+	breathFire->addAnimation(GoDown, 5, 37, 1);
+	breathFire->addAnimation(Attack, 5, 37, 1);
+
+	std::vector<Entity*>* entities = new std::vector<Entity*>{ gothicHero, demonAttack, hellBeast, ghost, breathFire };
 	std::vector<EState>* next = new std::vector<EState>{ none };
 
 	std::map<EState, State*>* states = new std::map<EState, State*>{ {mainMenu, new State{"MainMenu", entities, next}}, 
